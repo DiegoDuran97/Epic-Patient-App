bcb1b19d339a1cac2554a83ca5f23385faee3d43
DiegoDuran97 <Diego.Duran@torontomu.ca>
2024-08-13 12:41:22 -0400

    Third Draft

--- src/App.svelte
+++ src/App.svelte
@@ -1,89 +1,103 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import {CLIENT_ID, CODE_VERIFIER_LOCAL_STORAGE_KEY, FHIR_BASE_URL, REDIRECT_URL, SMART_AUTH_URL, SMART_TOKEN_URL} from "./config"
+  import { CLIENT_ID, CODE_VERIFIER_SESSION_STORAGE_KEY, FHIR_BASE_URL, REDIRECT_URL, SMART_AUTH_URL, SMART_TOKEN_URL, TOKEN_RESPONSE_SESSION_STORAGE_KEY } from "./config";
   import axios from 'axios';
   import pkceChallenge from 'pkce-challenge';
+  import PatientDetails from "./lib/PatientDetails.svelte";
 
-  let tokenResponse: {
-      access_token: string,
-      id_token: string,
-      patient: string,
-      scope: string
-    }
-  let loading = true
+  let tokenResponse: { access_token: string, id_token: string, patient: string, scope: string } | undefined;
+  let loading = true;
 
- 
   const generateCodeChallenge = async () => {
-    const {code_challenge, code_verifier} = await  pkceChallenge()
-    localStorage.setItem(CODE_VERIFIER_LOCAL_STORAGE_KEY, code_verifier)
-    return code_challenge
-  }
-
+    const { code_challenge, code_verifier } = await pkceChallenge();
+    sessionStorage.setItem(CODE_VERIFIER_SESSION_STORAGE_KEY, code_verifier);
+    return code_challenge;
+  };
 
   const generateRedirectedURL = (codeChallenge: string) => {
-   const authorizationURL = new URL(SMART_AUTH_URL)
-   authorizationURL.searchParams.set('clinet_id', CLIENT_ID)
-   authorizationURL.searchParams.set('scope', 'openid fhirUser')
-   authorizationURL.searchParams.set('redirect_uri', REDIRECT_URL)
-   authorizationURL.searchParams.set('response_type', 'code')
-   authorizationURL.searchParams.set('state', '')
-   authorizationURL.searchParams.set('aud', FHIR_BASE_URL)
-   authorizationURL.searchParams.set('code_challenge',  codeChallenge)
-   authorizationURL.searchParams.set('code_challenge_method', 'S256')
-  
-    return authorizationURL.href
+    const authorizationURL = new URL(SMART_AUTH_URL);
+    authorizationURL.searchParams.set('client_id', CLIENT_ID);
+    authorizationURL.searchParams.set('scope', 'openid fhirUser');
+    authorizationURL.searchParams.set('redirect_uri', REDIRECT_URL);
+    authorizationURL.searchParams.set('response_type', 'code');
+    authorizationURL.searchParams.set('state', '');
+    authorizationURL.searchParams.set('aud', FHIR_BASE_URL);
+    authorizationURL.searchParams.set('code_challenge', codeChallenge);
+    authorizationURL.searchParams.set('code_challenge_method', 'S256');
+    return authorizationURL.href;
+  };
 
-  }
-  const makeTokenRequest = async (code:string, codeVerifier:string) => {
+  const makeTokenRequest = async (code: string, codeVerifier: string) => {
     const tokenRequestForm = new FormData();
-    tokenRequestForm.set('grant_type', 'authorization_code') 
-    tokenRequestForm.set('code', code)
-    tokenRequestForm.set('redirect_uri', REDIRECT_URL)
-    tokenRequestForm.set('clinet_id', CLIENT_ID)
-    tokenRequestForm.set('code_verifier', codeVerifier)
+    tokenRequestForm.set('grant_type', 'authorization_code');
+    tokenRequestForm.set('code', code);
+    tokenRequestForm.set('redirect_uri', REDIRECT_URL);
+    tokenRequestForm.set('client_id', CLIENT_ID);
+    tokenRequestForm.set('code_verifier', codeVerifier);
 
+    try {
+      const response = await axios.post(SMART_TOKEN_URL, tokenRequestForm);
+      console.log("Response Data:", response.data);
 
-    const response = await axios.postForm(SMART_TOKEN_URL, tokenRequestForm)
-    console.log(response)
-    tokenResponse = response.data 
-  }
-
-    onMount(async () => {
-      const code = new URL(window.location.href).searchParams.get('code')
-      const codeVerifier = localStorage.getItem(CODE_VERIFIER_LOCAL_STORAGE_KEY) 
-      try {
-        if (code  && codeVerifier) {
-          await makeTokenRequest(code, codeVerifier)
-          localStorage.removeItem(CODE_VERIFIER_LOCAL_STORAGE_KEY)
-      } 
-    } catch (e) {
-      console.error(e)
+      if (response.data && typeof response.data === 'object') {
+        tokenResponse = response.data;
+        sessionStorage.setItem(TOKEN_RESPONSE_SESSION_STORAGE_KEY, JSON.stringify(tokenResponse));
+        console.log("Token saved to sessionStorage:", JSON.stringify(tokenResponse));
+        console.log("SessionStorage Token Response:", sessionStorage.getItem(TOKEN_RESPONSE_SESSION_STORAGE_KEY));
+      } else {
+        console.error("Invalid token response format:", response.data);
+      }
+    } catch (error) {
+      console.error("Error in token request:", error);
     }
-    loading = false
-  })
+  };
 
+  onMount(() => {
+    const code = new URL(window.location.href).searchParams.get('code');
+    const codeVerifier = sessionStorage.getItem(CODE_VERIFIER_SESSION_STORAGE_KEY);
+    const tokenResponseString = sessionStorage.getItem(TOKEN_RESPONSE_SESSION_STORAGE_KEY);
 
+    console.log("Code:", code);
+    console.log("Code Verifier:", codeVerifier);
+    console.log("Token Response String:", tokenResponseString);
 
-const initiateAuthorizationRequest = async () => {
-  const codeChallenge = await generateCodeChallenge()
-  const redirectUrl = generateRedirectedURL(codeChallenge)
-  window.location.href = redirectUrl
-}
+    if (tokenResponseString) {
+      try {
+        console.log("Trying to parse Token Response String:", tokenResponseString);
+        tokenResponse = JSON.parse(tokenResponseString);
+        console.log("Parsed Token Response:", tokenResponse);
+      } catch (e) {
+        console.error("JSON parsing error:", e, "Token Response String:", tokenResponseString);
+      }
+    } else if (code && codeVerifier) {
+      makeTokenRequest(code, codeVerifier).then(() => {
+        sessionStorage.removeItem(CODE_VERIFIER_SESSION_STORAGE_KEY);
+      }).catch((e) => {
+        console.error("Error during token request:", e);
+      });
+    }
+
+    loading = false;
+  });
+
+  const initiateAuthorizationRequest = async () => {
+    const codeChallenge = await generateCodeChallenge();
+    const redirectUrl = generateRedirectedURL(codeChallenge);
+    console.log("Redirecting to:", redirectUrl);
+    window.location.href = redirectUrl;
+  };
 </script>
 
 <main>
-
   {#if loading}
     Loading...
   {:else}
     {#if tokenResponse}
-      {JSON.stringify(tokenResponse)}
+      <PatientDetails accessToken={tokenResponse.access_token} patientId={tokenResponse.patient} />
     {:else}
-    <div class="flex justify-center my-20">
-    <button on:click={initiateAuthorizationRequest} class ="p-3 bg-black rounded-md text-white">Sign In With Epic</button>
-    </div>
-  {/if}
+      <div class="flex justify-center my-20">
+        <button on:click={initiateAuthorizationRequest} class="p-3 bg-black rounded-md text-white">Sign In With Epic</button>
+      </div>
+    {/if}
   {/if}
 </main>
-
-
--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@
 
  export const SMART_AUTH_URL = "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/authorize"
  export const SMART_TOKEN_URL = "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/token"
- export const REDIRECT_URL = 'http:localhost:5173'
- export const CODE_VERIFIER_LOCAL_STORAGE_KEY = 'smart_code_verifier'
+ export const REDIRECT_URL = 'http://localhost:5173'
+ export const CODE_VERIFIER_SESSION_STORAGE_KEY = 'smart_code_verifier'
 
- export const TOKEN_RESPONSE_LOCAL_STORAGE_KEY = 'smart_token_response'
+ export const TOKEN_RESPONSE_SESSION_STORAGE_KEY = 'smart_token_response'
